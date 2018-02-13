808d98849c8212b4b25d4dfa34e859d1a54811e8
Rodolfo <chavesrodolfo@gmail.com>
2018-02-13 01:34:34 -0200

    add materialize css

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                <h2>
-                    <a href="/">Landing</a> |
-                    <a href="/surveys"> Dashboard</a> |
-                    <a href="/surveys/new"> New Survey</a>
-                </h2>
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="/" className="brand-logo">Emaily</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="/surveys">Dashboard</a></li>
+                        <li><a href="/surveys/new">New Survey</a></li>
+                        <li><a href="/">Log in with Google</a></li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
