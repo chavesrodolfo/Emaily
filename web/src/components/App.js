908bba130bcb6a5f183e64d7f1aa8bd581eeb708
Rodolfo <chavesrodolfo@gmail.com>
2018-02-14 01:52:05 -0200

    call action - redux

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from "react-redux";
+import * as actions from '../actions';
+
 import Header from './Header';
 import Dashboard from './Dashboard';
 import SurveyNew from './SurveyNew';
 import Landing from './Landing';
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, actions)(App);
